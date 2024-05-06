5a8e39a2da1ea37cd25b92f4126b0c6f6d7db49f
aexces <aexces@protonmail.com>
2024-05-06 13:20:48 +0530

    cleared project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import Heading from "./components/heading.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Heading />
   </React.StrictMode>
 );
