f52a7e07e4a7ebf9fc9b276dc68868af3c77c1a3
aexces <aexces@protonmail.com>
2024-06-25 17:33:19 +0530

    removed

--- src/presentation/components/ResponsiveBuilder/ResponsiveProps.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-type ResponsiveProps = {
-  mobile: ReactNode | null;
-  tablet: ReactNode | null;
-  desktop: ReactNode | null;
-};
-export default ResponsiveProps;
